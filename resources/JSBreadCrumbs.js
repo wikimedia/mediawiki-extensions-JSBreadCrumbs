2754c542ca96b04b64efd632cc8d2a1ccda0eeb2
Cindy Cicalese <cindom@gmail.com>
2017-11-02 00:10:22 +0000

    Refactored.
    Change-Id: If08cc5bfe5b9ecf566e6be8f658d970a666e78e0

---
+++ resources/JSBreadCrumbs.js
@@ -0,0 +1,143 @@
+var jsbreadcrumbs_controller = ( function( mw, $ ) {
+	'use strict';
+
+	return {
+
+		initialize: function() {
+
+			var specialPageName = mw.config.get('wgCanonicalSpecialPageName');
+			if ( specialPageName === "Userlogout" ) {
+				$.cookie( 'mwext-jsbreadcrumbs', null, { path: '/' } );
+				return;
+			}
+
+			var config = mw.config.get( 'JSBreadCrumbs' );
+			var showSidebar = config.ShowSidebar;
+			var siteMaxCrumbs = config.SiteMaxCrumbs;
+			var globalMaxCrumbs = config.GlobalMaxCrumbs;
+			var separator = config.Separator;
+			var leadingDescription = config.LeadingDescription;
+			var pageName = config.PageName;
+
+			// get the breadcrumbs from the cookie
+			var breadcrumbs = $.cookie( 'mwext-jsbreadcrumbs' );
+			if ( breadcrumbs ) {
+				try {
+					breadcrumbs = JSON.parse( breadcrumbs );
+				} catch ( e ) {
+					breadcrumbs = [];
+				}
+			} else {
+				breadcrumbs = [];
+			}
+
+			// remove this URL from the breadcrumb list if it is already in it
+			var url = location.pathname + location.search;
+			var index = 0;
+			while ( index < breadcrumbs.length ) {
+				if ( breadcrumbs[index].url == url ) {
+					breadcrumbs.splice( index, 1 );
+				} else {
+					index++;
+				}
+			}
+
+			// add the current URL to the breadcrumb list if it points
+			// to a valid page
+			var siteName = mw.config.get( 'wgSiteName' );
+			if ( !pageName.endsWith( "Badtitle" ) ) {
+				breadcrumbs.push( {
+					url: url,
+					title: pageName,
+					siteName: siteName
+				} );
+			}
+
+			// get the list of breadcrumbs to display
+			var visibleCrumbs = [];
+			for (index = breadcrumbs.length - 1; index >= 0; index--) {
+				if ( breadcrumbs[index].siteName === siteName ) {
+					if ( visibleCrumbs.length < siteMaxCrumbs ) {
+						var breadcrumb = breadcrumbs[index];
+						var link = '<a href="' + breadcrumb.url + '">' + breadcrumb.title +
+							'</a>';
+						visibleCrumbs.push( link );
+					} else {
+						breadcrumbs.splice( index, 1 );
+					}
+				}
+			}
+
+			// truncate the breadcrumb list if necessary
+			if ( breadcrumbs.length > globalMaxCrumbs ) {
+				breadcrumbs = breadcrumbs.slice( breadcrumbs.length - globalMaxCrumbs );
+			}
+
+			// save the breadcrumbs to the cookie
+			$.cookie( 'mwext-jsbreadcrumbs', JSON.stringify( breadcrumbs ),
+				{ path: '/', expires: 30 } );
+
+			var skin = mw.config.get( 'skin' );
+
+			// insert breadcrumbs in the sidebar
+			if( showSidebar === true ) {
+				if ( skin === "vector" ) {
+					this.postVector( leadingDescription );
+				} else if ( skin === "modern" ) {
+					this.postOther( '#mw_portlets', leadingDescription );
+				} else if ( skin === "monobook" ) {
+					this.postOther( '#column-one', leadingDescription );
+				} else if ( skin === "cologneblue" ) {
+					this.postOther( '#quickbar', leadingDescription );
+				}
+
+				for ( index = 0; index < visibleCrumbs.length; index++ ) {
+						$("#p-rv-list2").append('<li>' + visibleCrumbs[index] + '</li>');
+				}
+
+			// insert breadcrumbs elsewhere
+			} else {
+
+				var cssSelector;
+				if ( skin === "foreground" ) {
+					cssSelector = "#mw-js-message";
+				} else {
+					cssSelector = "#top";
+				}
+
+				$( cssSelector ).before(
+					'<span id="mwext-bc" class="noprint plainlinks jsbc-breadcrumbs"></span>' );
+				var mwextbc = $( "#mwext-bc" );
+				mwextbc.append( leadingDescription + ': ' );
+				for ( index = visibleCrumbs.length - 1; index >= 0; index-- ) {
+					mwextbc.append( visibleCrumbs[index] );
+					if ( index > 0 ) {
+						mwextbc.append( ' ' + separator + ' ' );
+					}
+				}
+			}
+		},
+
+		postVector: function( leadingDescription ) {
+			$("#mw-panel").append("<div class='portal persistent' role='navigation' id='p-rv' aria-labelledby='p-rv-label'></div>");
+			$("#p-rv").append("<h3 id='p-rv-label' tabindex='3'><a href='#' aria-haspopup='true' aria-controls='p-rv-list2' role='button' aria-pressed='false' aria-expanded='true'>" + leadingDescription + "</a></h3>");
+			$("#p-rv").append("<div class='body' style='display: block;'><ul id='p-rv-list2'></ul>");
+		},
+
+		postOther: function(id, leadingDescription ) {
+			$(id).append("<div class='portlet' id='p-rv' role='navigation'></div>");
+			$("#p-rv").append("<h3>" + leadingDescription + "</h3><div class='pBody'><ul id='p-rv-list'></ul></div>");
+		}
+	}
+}( mediaWiki, jQuery ) );
+
+window.JSBreadCrumbsController = jsbreadcrumbs_controller;
+
+( function( mw, $ ) {
+	$( document )
+		.ready( function() {
+			if ( mw.config.exists( 'JSBreadCrumbs' ) ) {
+				window.JSBreadCrumbsController.initialize();
+			}
+		} );
+}( mediaWiki, jQuery ) );
