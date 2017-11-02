eb2cf899b2946b59ef6d4a707eb494279240ca6e
Cindy Cicalese <cindom@gmail.com>
2017-11-02 00:10:22 +0000

    Refactored.
    - converted to extension registration
    - localized hard-coded English text
    - replaced non-working pervasive wiki farm option with
      jsbreadcrumbs-domain user option
    - reworked CSS options
    - added global maximum crumbs in addition to site maximum
    - made maximum length of displayed crumbs configurable
    - optionally added localized action name to the end of crumb for action other
    	than view (e.g. edit, history)
    
    Change-Id: If08cc5bfe5b9ecf566e6be8f658d970a666e78e0

---
+++ includes/JSBreadCrumbsHooks.php
@@ -0,0 +1,168 @@
+<?php
+
+class JSBreadCrumbsHooks {
+
+	/**
+	 * Implements BeforePageDisplay hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
+	 * Initializes variables to be passed to JavaScript.
+	 *
+	 * @param OutputPage $output OutputPage object
+	 * @param Skin $skin Skin object that will be used to generate the page
+	 */
+	public static function onBeforePageDisplay( $output, $skin ) {
+		$user = $output->getUser();
+		if ( !$user->isAllowed( 'read' ) ||
+			!$user->getOption( 'jsbreadcrumbs-showcrumbs' ) ) {
+			return;
+		}
+
+		$vars = [];
+
+		$vars['SiteMaxCrumbs'] = $user->getOption( 'jsbreadcrumbs-numberofcrumbs' );
+		$vars['GlobalMaxCrumbs'] = $GLOBALS['wgJSBreadCrumbsGlobalMaxCrumbs'];
+
+		$vars['ShowAction'] = (bool)$user->getOption( 'jsbreadcrumbs-showaction' );
+		$vars['ShowSite'] = (bool)$user->getOption( 'jsbreadcrumbs-showsite' );
+		$vars['Domain'] = (bool)$user->getOption( 'jsbreadcrumbs-domain' );
+
+		// Allow localized horizontal separator to be overriden
+		if ( $GLOBALS['wgJSBreadCrumbsHorizontalSeparator'] !== '' ) {
+			$vars['HorizontalSeparator'] =
+				$GLOBALS['wgJSBreadCrumbsHorizontalSeparator'];
+		} else {
+			$vars['HorizontalSeparator'] =
+				wfMessage( 'jsbreadcrumbs-horizontal-separator' )->escaped();
+		}
+
+		$horizontal =
+			(bool)$user->getOption( 'jsbreadcrumbs-horizontal' );
+		$vars['Horizontal'] = $horizontal;
+		if ( $horizontal ) {
+			$vars['CSSSelector'] = $GLOBALS['wgJSBreadCrumbsCSSSelectorHorizontal'];
+			$vars['LeadingDescription'] = wfMessage( 'jsbreadcrumbs-intro-horizontal',
+				$vars['SiteMaxCrumbs'] )->plain();
+			$vars['MaxLength'] =
+				$user->getOption( 'jsbreadcrumbs-maxlength-horizontal' );
+		} else {
+			$vars['CSSSelector'] = $GLOBALS['wgJSBreadCrumbsCSSSelectorVertical'];
+			$vars['LeadingDescription'] = wfMessage( 'jsbreadcrumbs-intro-vertical',
+				$vars['SiteMaxCrumbs'] )->plain();
+			$vars['MaxLength'] =
+				$user->getOption( 'jsbreadcrumbs-maxlength-vertical' );
+		}
+
+		$title = $output->getTitle();
+		if ( self::getDisplayTitle( $title, $displayTitle ) ) {
+			$pagename = $displayTitle;
+		} else {
+			$pagename = $title->getPrefixedText();
+		}
+		$vars['PageName'] = $pagename;
+
+		$action = Action::getActionName( $output->getContext() );
+		if ( $action === 'view' ) {
+			$vars['Action'] = '';
+		} else {
+			$message = wfMessage( $action );
+			if ( $message->isBlank() ) {
+				$vars['Action'] = $action;
+			} else {
+				$vars['Action'] = $message->plain();
+			}
+		}
+
+		$output->addJSConfigVars( 'JSBreadCrumbs', $vars );
+		$output->addModules( 'ext.JSBreadCrumbs' );
+	}
+
+	/**
+	 * GetPreferences hook
+	 *
+	 * @param User $user User whose preferences are being modified
+	 * @param array &$preferences Preferences description array
+	 */
+	public static function onGetPreferences( User $user, array &$preferences ) {
+		$preferences['jsbreadcrumbs-showcrumbs'] = [
+			'type' => 'toggle',
+			'label-message' => 'prefs-jsbreadcrumbs-showcrumbs',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$max = $GLOBALS['wgJSBreadCrumbsGlobalMaxCrumbs'];
+		$preferences['jsbreadcrumbs-numberofcrumbs'] = [
+			'type' => 'int',
+			'min' => 1,
+			'max' => $max,
+			'label-message' => 'prefs-jsbreadcrumbs-numberofcrumbs',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-horizontal'] = [
+			'type' => 'toggle',
+			'label-message' => 'prefs-jsbreadcrumbs-horizontal',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-maxlength-horizontal'] = [
+			'type' => 'int',
+			'min' => 0,
+			'max' => 100,
+			'label-message' => 'prefs-jsbreadcrumbs-maxlength-horizontal',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-maxlength-vertical'] = [
+			'type' => 'int',
+			'min' => 0,
+			'max' => 100,
+			'label-message' => 'prefs-jsbreadcrumbs-maxlength-vertical',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-showaction'] = [
+			'type' => 'toggle',
+			'label-message' => 'prefs-jsbreadcrumbs-showaction',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-showsite'] = [
+			'type' => 'toggle',
+			'label-message' => 'prefs-jsbreadcrumbs-showsite',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+
+		$preferences['jsbreadcrumbs-domain'] = [
+			'type' => 'toggle',
+			'label-message' => 'prefs-jsbreadcrumbs-domain',
+			'section' => 'rendering/jsbreadcrumbs'
+		];
+	}
+
+	/**
+	 * Get displaytitle page property text.
+	 *
+	 * @since 1.0
+	 * @param Title $title the Title object for the page
+	 * @param string &$displaytitle to return the display title, if set
+	 * @return boolean true if the page has a displaytitle page property that is
+	 * different from the prefixed page name, false otherwise
+	 */
+	private static function getDisplayTitle( Title $title, &$displaytitle ) {
+		$pagetitle = $title->getPrefixedText();
+		$title = $title->createFragmentTarget( '' );
+		if ( $title instanceof Title ) {
+			$values = PageProps::getInstance()->getProperties( $title, 'displaytitle' );
+			$id = $title->getArticleID();
+			if ( array_key_exists( $id, $values ) ) {
+				$value = $values[$id];
+				if ( trim( str_replace( '&#160;', '', strip_tags( $value ) ) ) !== '' &&
+					$value !== $pagetitle ) {
+					$displaytitle = $value;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ resources/JSBreadCrumbs.js
@@ -0,0 +1,160 @@
+var jsbreadcrumbs_controller = ( function( mw, $ ) {
+	'use strict';
+
+	return {
+
+		initialize: function() {
+
+			var config = mw.config.get( 'JSBreadCrumbs' );
+			var pageName = config.PageName;
+			var action = config.Action;
+			var maxLength = config.MaxLength;
+			var siteMaxCrumbs = config.SiteMaxCrumbs;
+			var globalMaxCrumbs = config.GlobalMaxCrumbs;
+			var domain = config.Domain;
+			var showAction = config.ShowAction;
+			var showSite = config.ShowSite;
+			var horizontal = config.Horizontal;
+			var horizontalSeparator = config.HorizontalSeparator;
+			var cssSelector = config.CSSSelector;
+			var leadingDescription = config.LeadingDescription;
+
+			var siteName = mw.config.get( 'wgSiteName' );
+
+			// get the breadcrumbs from the cookie
+			var breadcrumbs = $.cookie( 'mwext-jsbreadcrumbs' );
+			if ( breadcrumbs ) {
+				try {
+					breadcrumbs = JSON.parse( breadcrumbs );
+				} catch ( e ) {
+					breadcrumbs = [];
+				}
+			} else {
+				breadcrumbs = [];
+			}
+
+			// remove this URL from the breadcrumb list if it is already in it
+			var url = location.pathname + location.search;
+			var index = 0;
+			while ( index < breadcrumbs.length ) {
+				if ( breadcrumbs[index].url === url ) {
+					breadcrumbs.splice( index, 1 );
+				} else {
+					index++;
+				}
+			}
+
+			// add the current URL to the breadcrumb list if it points
+			// to a valid page
+			if ( !pageName.endsWith( "Badtitle" ) ) {
+				breadcrumbs.push( {
+					url: url,
+					title: pageName,
+					action: action,
+					siteName: siteName
+				} );
+			}
+
+			// get the list of breadcrumbs to display
+			var visibleCrumbs = [];
+			for (index = breadcrumbs.length - 1; index >= 0; index--) {
+				if ( domain || breadcrumbs[index].siteName === siteName ) {
+					if ( visibleCrumbs.length < siteMaxCrumbs ) {
+						var breadcrumb = breadcrumbs[index];
+						if ( ! ('action' in breadcrumb ) || showAction ||
+							breadcrumb.action.length === 0 ) {
+							var link = '<a href="' + breadcrumb.url + '">';
+							if ( showSite ) {
+								link += breadcrumb.siteName + ': ';
+							}
+							var title = breadcrumb.title;
+							if ( title.length > maxLength ) {
+								title = title.substr( 0, maxLength ) + '...';
+							}
+							if ( 'action' in breadcrumb && breadcrumb.action.length > 0 ) {
+								title += ' [' + breadcrumb.action + ']';
+							}
+							link += title + '</a>';
+							visibleCrumbs.push( link );
+						} else {
+							breadcrumbs.splice( index, 1 );
+						}
+					} else {
+						breadcrumbs.splice( index, 1 );
+					}
+				}
+			}
+
+			// truncate the breadcrumb list if necessary
+			if ( breadcrumbs.length > globalMaxCrumbs ) {
+				breadcrumbs = breadcrumbs.slice( breadcrumbs.length - globalMaxCrumbs );
+			}
+
+			// save the breadcrumbs to the cookie
+			$.cookie( 'mwext-jsbreadcrumbs', JSON.stringify( breadcrumbs ),
+				{ path: '/', expires: 30 } );
+
+			var skin = mw.config.get( 'skin' );
+
+			var selector;
+			if( horizontal ) {
+				if ( skin in cssSelector ) {
+					selector = cssSelector[skin];
+				} else if ( skin === "foreground" ) {
+					selector = "#mw-js-message";
+				} else {
+					selector = "#top";
+				}
+
+				$( selector ).before(
+					'<span id="mwext-bc" class="noprint plainlinks jsbc-breadcrumbs"></span>' );
+				var mwextbc = $( "#mwext-bc" );
+				mwextbc.append( leadingDescription);
+				for ( index = visibleCrumbs.length - 1; index >= 0; index-- ) {
+					mwextbc.append( visibleCrumbs[index] );
+					if ( index > 0 ) {
+						mwextbc.append( ' ' + horizontalSeparator + ' ' );
+					}
+				}
+			} else {
+				if ( skin in cssSelector ) {
+					selector = cssSelector[skin];
+				} else if ( skin === "vector" ) {
+					selector = "#p-tb";
+				} else if ( skin === "modern" ) {
+					selector = '#mw_portlets';
+				} else if ( skin === "monobook" ) {
+					selector = '#column-one';
+				} else if ( skin === "cologneblue" ) {
+					selector = '#quickbar';
+				} else {
+					selector = '#column-one';
+				}
+
+				if ( skin === "vector" ) {
+					$(selector).after("<div class='portal' id='p-rv' role='navigation' aria-labelledby='p-rv-label'></div>");
+					$("#p-rv").append("<h3 id='p-rv-label'>" + leadingDescription + "</h3>");
+					$("#p-rv").append("<div class='body' style='display: block;'><ul id='p-rv-list2'></ul>");
+				} else {
+					$(selector).append("<div class='portlet' id='p-rv' role='navigation'></div>");
+					$("#p-rv").append("<h3>" + leadingDescription + "</h3><div class='pBody'><ul id='p-rv-list'></ul></div>");
+				}
+
+				for ( index = 0; index < visibleCrumbs.length; index++ ) {
+						$("#p-rv-list2").append('<li>' + visibleCrumbs[index] + '</li>');
+				}
+			}
+		}
+	};
+}( mediaWiki, jQuery ) );
+
+window.JSBreadCrumbsController = jsbreadcrumbs_controller;
+
+( function( mw, $ ) {
+	$( document )
+		.ready( function() {
+			if ( mw.config.exists( 'JSBreadCrumbs' ) ) {
+				window.JSBreadCrumbsController.initialize();
+			}
+		} );
+}( mediaWiki, jQuery ) );
